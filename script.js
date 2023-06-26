a36fe718fb64ea2c6d8230941a13602cc5b5cf46
Ahmed Bakr <117715752+Ahmedbakr74@users.noreply.github.com>
2023-06-26 18:45:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+
+function click0(){
+    document.getElementById("text").value += document.getElementById("num0").value;
+}
+function click1(){
+    document.getElementById("text").value += document.getElementById("num1").value;
+}
+function click2(){
+    document.getElementById("text").value += document.getElementById("num2").value;
+}
+function click3(){
+    document.getElementById("text").value += document.getElementById("num3").value;
+}
+function click4(){
+    document.getElementById("text").value += document.getElementById("num4").value;
+}
+function click5(){
+    document.getElementById("text").value += document.getElementById("num5").value;
+}
+function click6(){
+    document.getElementById("text").value += document.getElementById("num6").value;
+}
+function click7(){
+    document.getElementById("text").value += document.getElementById("num7").value;
+}
+function click8(){
+    document.getElementById("text").value += document.getElementById("num8").value;
+}
+function click9(){
+    document.getElementById("text").value += document.getElementById("num9").value;
+}
+function clicks(){
+    document.getElementById("text").value += document.getElementById("sum").value;
+}
+function clickm(){
+    document.getElementById("text").value += document.getElementById("mins").value;
+}
+function clickX(){
+    document.getElementById("text").value += document.getElementById("num*").value;
+}
+
+function clickeq(){
+    document.getElementById("text").value = eval(document.getElementById("text").value);
+}
+function clickprec(){
+    document.getElementById("text").value += document.getElementById("precent").value;
+   
+}
+function clickdivison(){
+    document.getElementById('text').value += document.getElementById("division").value;
+}
+function clickous(){
+    document.getElementById("text").value += document.getElementById("ous").value;
+}
+function clickinfo(){
+    confirm( "My Name Is: Ahmed Reda Bakr ")
+}
+function clickc(){
+    document.getElementById( "text").value="" ;
+}
+function clickdot(){
+    document.getElementById("text").value += document.getElementById("dot").value;
+}
+
+
